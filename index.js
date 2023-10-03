8e5a349f6587c0b9a2ed50305fb39c972045a6a4
Rashmi028 <vermarashmi028@gmail.com>
2023-10-04 03:49:41 +0530

    ToDO-List

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-import express from "express"
+import express from "express";
+import bodyParser from "body-parser";
+import moment from "moment";
+
 const app=express();
-const port=3000;
+const port=3001;
+app.use(express.static("public"));
+app.use((req, res, next)=>{
+    res.locals.moment = moment;
+    next();
+  });
 app.get("/",(req,res)=>{
     res.render("index.ejs");
 });
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
+});
+app.get("/today",(req,res)=>{
+    res.render("today.ejs");
+});
+app.get("/work",(req,res)=>{
+    res.render("work.ejs");
+});
+
 app.post("/submit",(req,res)=>{
     res.render("index.ejs",{
-        var 
-  if()
     });
 });
 app.listen(port, () => {
